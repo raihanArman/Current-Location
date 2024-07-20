a09ce93b22665c0ba9da1dca07dafd44c3b46b02
raihanArman <raihanarman8@gmail.com>
2024-07-20 10:59:43 +0700

    Initial Commit

---
+++ Current Location/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Current Location
+//
+//  Created by Raihan Arman on 20/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Current Location/Current_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Current_LocationApp.swift
+//  Current Location
+//
+//  Created by Raihan Arman on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Current_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
